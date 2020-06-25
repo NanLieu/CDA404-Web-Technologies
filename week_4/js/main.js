cefb6f81759298123d591529ea067bf321dfcbdc
NanLieu <4lieun20@solent.ac.uk>
2020-06-25 14:27:46 +0100

    Week 4 topic 2 task 1 progress

---
+++ week_4/js/main.js
@@ -0,0 +1,11 @@
+var random_number
+var number_of_guesses
+
+function getRandomArbitrary(min, max)
+
+function main() {
+    alert('working')
+    random_number = getRandomArbitrary
+}
+
+Math.round(Math.random() * (max - min) + min);
